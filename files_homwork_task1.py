589fce229e01f6147a62e34d4d26d6a0895a21ae
rslproj21 <111995004+rslproj21@users.noreply.github.com>
2022-10-17 11:09:10 +0300

    Update
    fix task 3

--- files_homwork_task1.py
+++ files_homwork_task1.py
@@ -36,36 +36,35 @@ def cook(dishes, person_count):
     for meals in eat:
         print(meals)
         
-file_len = []
-countt = 0
+files = []
+counting = 0
 
-def file_print(file, f):
-    a = file.name.replace('.txt', '')
+def data_output(file, some_file):
+    replace_view = file.name.replace('.txt', '')
     print(file.name)
-    print((len(f)))
+    print((len(some_file)))
     count = 1
-    for i in f:
-        print(f'Строка номер {count} файл номер {a}')
+    for ingor in some_file:
+        print(f'Строка номер: {count}, файл номер: {replace_view}')
         count += 1
     count = 1
 
 with open('1.txt', 'rt') as file:
     f = file.readlines()
-    file_len.append(len(f))
-with open('2.txt') as file2:
-    f2 = file2.readlines()
-    file_len.append(len(f2))
-with open('3.txt') as file3:
-    f3 = file3.readlines()
-    file_len.append(len(f3))
+    files.append(len(f))
+with open('2.txt') as file_2:
+    f2 = file_2.readlines()
+with open('3.txt') as file_3:
+    f3 = file_3.readlines()
+    files.append(len(f3))
 
-
-while countt <= 2:
-    if len(f) == min(file_len):
-        file_print(file, f)
-    if len(f2) == min(file_len):
-        file_print(file2, f2)
-    if len(f3) == min(file_len):
-        file_print(file3, f3)
-    file_len.remove(min(file_len))
-    countt += 1
+while counting <= 2:
+    if len(f) == min(files):
+        data_output(file, f)
+    if len(f2) == min(files):
+        data_output(file, f2)
+    if len(f3) == min(files):
+        data_output(file, f3)
+    files.remove(min(files))
+    counting += 1        
+        
